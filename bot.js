9ad9e1f84647e275e3a1a86aefcfd8fdb199419e
Dragon256 <40772125+Dragon256@users.noreply.github.com>
2018-07-02 23:48:08 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -83,4 +83,76 @@ client.on('message', function(msg) {
   });
 
 
+
+
+
+
+client.on('message', async message =>{
+  var prefix = "-";
+
+const ms = require("ms");
+if (message.author.omar) return;
+if (!message.content.startsWith(-mute)) return;
+if(!message.channel.guild) return message.channel.send('**هذا الأمر فقط للسيرفرات**').then(m => m.delete(5000)); 
+if(!message.member.hasPermission('ADMINISTRATOR')) return message.reply (" :negative_squared_cross_mark: :هذا الخاصيه للادمن فقط").then(msg => msg.delete(5000));;
+var command = message.content.split(" ")[0];
+command = command.slice(prefix.length);
+var args = message.content.split(" ").slice(1);
+    if(command == "mute") {
+    let tomute = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
+if(tomute.hasPermission("MANAGE_MESSAGES"))return      message.channel.send('**للأسف لا أمتلك صلاحية ** `MANAGE_MASSAGEES`');
+    let muterole = message.guild.roles.find(`name`, "Muted");
+    if(!muterole){
+      try{
+        muterole = await message.guild.createRole({
+          name: "Muted",
+          color: "#000000",
+          permissions:[]
+        })
+        message.guild.channels.forEach(async (channel, id) => {
+          await channel.overwritePermissions(muterole, {
+            SEND_MESSAGES: false,
+            ADD_REACTIONS: false
+          });
+        });
+      }catch(e){
+        console.log(e.stack);
+      }
+    }
+    
+        let mutetime = args[1];
+    if(!mutetime) return message.reply("**يرجى تحديد وقت الميوت**:x:");  
+    await(tomute.addRole(muterole.id));
+    message.channel.send(`Member <@${tomute.id}>has been muted ${ms(ms(mutetime))} :white_check_mark: . 
+ By: <@${message.author.id}> 
+
+`); 
+setTimeout(function(){
+      tomute.removeRole(muterole.id);    
+      message.channel.send(` :white_check_mark: <@${tomute.id}> **لقد انتهي الميوت للشخص** `);  
+    }, ms(mutetime));
+  
+  
+
+  }
+if(command === `unmute`) {
+  if(!message.member.hasPermission("MANAGE_ROLES")) return message.channel.sendMessage("**ليس لديك صلاحية لفك عن الشخص ميوت**:x: ").then(m => m.delete(5000));
+  let toMute = message.guild.member(message.mentions.users.first()) || message.guild.members.get(args[0]); 
+  if(!toMute) return message.channel.sendMessage("**عليك المنشن أولاّ**:x: "); 
+
+  let role = message.guild.roles.find (r => r.name === "Muted");
+  
+  if(!role || !toMute.roles.has(role.id)) return message.channel.sendMessage("**لم يتم اعطاء هذه شخص ميوت من الأساس**:x:") 
+
+  await toMute.removeRole(role)
+  message.channel.sendMessage(":white_check_mark: :**لقد تم فك الميوت عن شخص بنجاح**");
+
+  return;
+
+  }
+
+});
+
+
+
 client.login(process.env.BOT_TOKEN);
