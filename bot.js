78e57e4a77ab3209b09b94f6ebdefda8eace75f4
Dragon256 <40772125+Dragon256@users.noreply.github.com>
2018-07-02 22:36:02 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -28,4 +28,35 @@ client.user.setGame(`I will be good soon !`,"http://twitch.tv/S-F")
   console.log('')
 });
 
+
+
+
+
+client.on('message', message => {
+   if (message.content.startsWith("-id")) {
+                if(!message.channel.guild) return message.reply('** This command only for servers :x: **');
+
+               var mentionned = message.mentions.users.first();
+    var mentionavatar;
+      if(mentionned){
+          var mentionavatar = mentionned;
+      } else {
+          var mentionavatar = message.author;
+          
+      }
+   let embed = new Discord.RichEmbed()
+  .setColor("RANDOM")
+   .setThumbnail(`${mentionavatar.avatarURL}`)
+  .addField("اسمك:",`<@+${mentionavatar.id}+>`, true)
+  .addField('التاج حقك:',"#" +`${mentionavatar.discriminator}`, true)
+   .addField("الايدي:", "**[" +`${mentionavatar.id}`+ "]**", true)
+  .addField("دخولك للديسكورد منذ:", "**[" +`${mentionavatar.createdAt}` + "]**", true)
+
+
+  message.channel.sendEmbed(embed);
+  console.log('[id] Send By: ' + message.author.username)
+    }
+});
+
+
 client.login(process.env.BOT_TOKEN);
