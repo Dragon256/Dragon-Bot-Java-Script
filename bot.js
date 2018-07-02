5ecf3da89d05529a8a9fddcc302b12e9ab52cc74
Dragon256 <40772125+Dragon256@users.noreply.github.com>
2018-07-03 00:10:10 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -101,4 +101,19 @@ message.channel.sendEmbed(embed);
 
 
 
+
+client.on('message', message => {
+  if (message.content === "-bot") {
+      if(!message.channel.guild) return;
+  let embed = new Discord.RichEmbed()
+  .setAuthor(` ${message.author.username} `, message.author.avatarURL)      
+  .addField(`عدد السيرفرات:${client.guilds.size}`,`عدد المستخدمين:${client.users.size}`)
+  .setThumbnail(message.author.avatarURL)
+  .addField(':small_blue_diamond:By', "<@" + message.author.id + ">")        
+message.channel.sendEmbed(embed);
+ }
+});
+
+
+
 client.login(process.env.BOT_TOKEN);
