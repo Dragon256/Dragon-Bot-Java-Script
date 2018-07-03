b2d5862374e5c3d4cc80da9e804cb443394ea527
Dragon256 <40772125+Dragon256@users.noreply.github.com>
2018-07-03 20:53:09 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -216,18 +216,17 @@ client.on('message', message => {
 
 
 
-var refix= "-";
-client.on("message", message => {
-    if(message.content.startsWith(prefix + 'v2min')) {
-     let args = message.content.split(" ").slice(1);
-       var nam = args.join(' ');
-    
-      if(!message.member.hasPermission('ADMINISTRATOR')) return    message.channel.send('`ADMINISTRATOR` للأسف هذه الخاصية تحتاج الى ').then(msg => msg.delete(6000))
-      if (!nam) return message.channel.send(`<@${message.author.id}> يجب عليك ادخال اسم`).then(msg => msg.delete(10000))
-      message.guild.createChannel(nam, 'voice').then(c => setTimeout(() => c.delete(), 120000))
-      message.channel.send(`:ballot_box_with_check: TemporarySound : \`${nam}\``).then(c => setTimeout(() => c.edit(`<@${message.author.id}> :stopwatch:  انتهى وقت الروم الصوتي`), 120000))  // 120000 دقيقتان
-    }
-    });
+client.on('guildMemberAdd', member => {
+    var embed = new Discord.RichEmbed()
+    .setThumbnail(member.user.avatarURL)
+  .addField("***شكرا الانضمامك الينا***" ,member.user.username )
+    .setDescription('***بكل حب واحترام وشوق نستقبلك ونتمنى لك قضآء أجمل اللحظات ولآوقات معنا***')
+    .setColor('RANDOM')
+    .setImage('http://www.imgion.com/images/01/Welcome-buddy.jpg')
+var channel =member.guild.channels.find('name', 'welcome')
+if (!channel) return;
+channel.send({embed : embed});
+});
 
 
 
