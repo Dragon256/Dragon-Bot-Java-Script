938bc63549c37a87ba291c7a913e73e1695faf59
Dragon256 <40772125+Dragon256@users.noreply.github.com>
2018-07-03 21:23:36 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -300,35 +300,29 @@ client.on("message", message => {
 
 
 client.on('message', message => {
-	var prefix = "-"
-    if (message.content.startsWith(prefix + "profile")) {
-var args = message.content.split(" ").slice(1);
-let user = message.mentions.users.first();
-var men = message.mentions.users.first();
- var heg;
- if(men) {
-     heg = men
- } else {
-     heg = message.author
- }
-var mentionned = message.mentions.members.first();
-  var h;
- if(mentionned) {
-     h = mentionned
- } else {
-     h = message.member
- }
-        moment.locale('ar-TN');
-var id = new  Discord.RichEmbed()
-.setColor("RANDOM")
-.addField(': انضمامك لسيرفر قبل', `${moment(h.joinedAt).format('YYYY/M/D HH:mm:ss')} \n \`${moment(h.joinedAt).fromNow()}\``, true)
-.addField(': دخولك لديسكورد قبل', `${moment(heg.createdTimestamp).format('YYYY/M/D HH:mm:ss')} **\n** \`${moment(heg.createdTimestamp).fromNow()}\`` ,true)
-.addField(": النك نيم",`${h.nickname}`, true) .addField(": #",heg.discriminator, true)
-.addField(`: البلينق`,`${h.presence.game && h.presence.game.name || '-'}`,true) .addField(': الحالة',`${h.presence.status}`,true)
-.addField(`: الرتب`, `${message.guild.members.get(h.id).roles.map(r => `\`${r.name}\``).slice(1).join('\n') || 'لايوجد رتب'}`,true)                                                    
-.setThumbnail(heg.avatarURL);
-message.channel.send(id)
-}       });
+
+    if (message.content === "-server") {
+var year = message.guild.createdAt.getFullYear()
+var month = message.guild.createdAt.getMonth()
+var day = message.guild.createdAt.getDate()
+    let embed = new Discord.RichEmbed()
+
+.addField('***صاحب السيرفر : ***: ' , message.guild.owner)
+.addField('***اسم السيرفر :***: ' , message.guild.name)
+.addField('***ايدي السيرفر :***:' , message.guild.id)
+.addField('***قوة الحماية :***: ' , message.guild.verificationLevel)
+.addField('***نطاق السيرفر :globe_with_meridians: ***: ' , message.guild.region)
+.addField('***عدد الرومات : ***: ' , message.guild.defaultChannel)
+.addField('***حجم الرومات ***: ' , message.guild.channels.size)
+.addField('***عدد الرتب : ***: ' , message.guild.roles.size)
+.addField('***عدد الاعضاء :***: ' , message.guild.memberCount)
+.addField('***تم صنع السيرفر في :***: ' ,year + "-"+ month +"-"+ day)
+.setColor("#51cde6")
+.setDescription(`${message.guild.name}`)
+     message.channel.sendEmbed(embed);
+} 
+
+});
 
 
 
