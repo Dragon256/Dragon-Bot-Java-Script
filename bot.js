e93a40eabbb165ba493ca58051594f4245048eea
Dragon256 <40772125+Dragon256@users.noreply.github.com>
2018-07-03 21:47:18 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -326,4 +326,34 @@ var day = message.guild.createdAt.getDate()
 
 
 
+client.on('message', function(message) {
+    if(!message.channel.guild) return;
+if(message.content ===  '-color 140') {
+if(message.member.hasPermission('MANAGE_ROLES')) {
+setInterval(function(){})
+message.channel.send('جاري عمل الالوان |✅')
+}else{
+message.channel.send(' ``` MANAGE_ROLES ``` انت لا تملك برمشن')
+}
+}
+});
+
+client.on('message', message=>{
+if (message.content ===  '-color 140'){
+if(!message.channel.guild) return;
+if (message.member.hasPermission('MANAGE_ROLES')){
+  setInterval(function(){})
+    let count = 0;
+    let ecount = 0;
+for(let x = 1; x < 141; x++){
+message.guild.createRole({name:x,
+color: 'RANDOM'})
+}
+}
+}
+});
+
+
+
+
 client.login(process.env.BOT_TOKEN);
