c092d2c7d694e8890db7ba41a373fe8a9e34bd37
Dragon256 <40772125+Dragon256@users.noreply.github.com>
2018-07-04 20:14:12 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -351,4 +351,27 @@ client.on('message', message => {
 
 
 
+client.on('message', message => { 
+    var refix = "-";
+    if (message.author.boss) return;
+    if (!message.content.startsWith(prefix)) return;
+    let command = message.content.split(" ")[0];
+    command = command.slice(prefix.length);
+    if (command == "role") {
+    if (!message.channel.guild) return;
+    if(!message.guild.member(message.author).hasPermission("MANAGE_ROLES")) return message.reply("** MANAGE_ROLES انت لا تملك برمشن  **").then(msg => msg.delete(5000));;
+    if(!message.guild.member(client.user).hasPermission("MANAGE_ROLES")) return message.reply(" MANAGE_ROLES البوت لا يملك برمشن  ").then(msg => msg.delete(5000));;
+    let user = message.mentions.users.first();
+    if (message.mentions.users.size < 1) return message.reply('**ضع منشن الشخص!!**').then(msg => {msg.delete(5000)});
+    let MRole = message.content.split(" ").slice(2).join(" ");
+    if(!MRole)return message.reply("يجب عليك وضع اسم الرتبة").then(msg => {msg.delete(5000)});
+    message.guild.member(user).addRole(message.guild.roles.find("name", MRole));
+    message.reply('*** تم اضافة الرتبة الى الشخص :white_check_mark:  ***').then(msg => {msg.delete(10000)});
+    }
+    });
+    
+
+
+
+
 client.login(process.env.BOT_TOKEN);
