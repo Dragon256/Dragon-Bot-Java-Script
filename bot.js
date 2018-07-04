93aa66eb82f9e6fe7109705363eed80c4936f53a
Dragon256 <40772125+Dragon256@users.noreply.github.com>
2018-07-04 23:53:21 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -337,55 +337,7 @@ client.on('guildMemberAdd', member=> {
 
 
 
-client.on('message', message => {
-    let args = message.content.split(' ').slice(1).join(' ');
-    if (message.content.startsWith('-bc-bot')){
-    if(!message.author.id === '') return;
-    message.channel.sendMessage('جار ارسال الرسالة :white_check_mark:')
-    client.users.forEach(m =>{
-    m.sendMessage(args)
-    })
-    }
-    });
 
-
-
-
-client.on('message', message => {
-const prefix = "-";
-    if (message.author.id === client.user.id) return;
-    if (message.guild) {
-   let embed = new Discord.RichEmbed()
-    let args = message.content.split(' ').slice(1).join(' ');
-if(message.content.split(' ')[0] == prefix + 'bc') {
-    if (!args[1]) {
-message.channel.send("**-bc <message>**");
-return;
-}
-        message.guild.members.forEach(m => {
-   if(!message.member.hasPermission('ADMINISTRATOR')) return;
-            var bc = new Discord.RichEmbed()
-            .setAuthor(message.author.username, message.author.avatarURL)
-            .addField('** الـسيرفر**', `${message.guild.name}`,true)
-            .addField(' **الـمرسل **', `${message.author.username}#${message.author.discriminator}`,true)
-            .addField(' **الرسالة** ', args)
-            .setThumbnail(message.guild.iconURL)
-            .setColor('RANDOM')
-            m.send(`${m}`,{embed: bc});
-        });
-        const AziRo = new Discord.RichEmbed()
-        .setAuthor(message.author.username, message.author.avatarURL)   
-        .setTitle('✔️ | جاري ارسال رسالتك ') 
-        .addBlankField(true)
-        .addField('👥 | عدد الاعضاء المرسل لهم ', message.guild.memberCount , true)        
-        .addField('📋| الرسالة ', args)
-        .setColor('RANDOM')  
-        message.channel.sendEmbed(AziRo);          
-    }
-    } else {
-        return;
-    }
-});
     
 
 
