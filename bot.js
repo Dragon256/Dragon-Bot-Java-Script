186d3d94acea5bc4dd6cb2009ee54d50d26529fe
Dragon256 <40772125+Dragon256@users.noreply.github.com>
2018-07-06 00:11:57 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -117,43 +117,7 @@ client.on('message',async message =>{
 
 
 
-let rebel;
-client.on("ready", async  => {
-    let guild = client.guilds.get("399274624669581335");
-  let users = guild.members.map(member => member.user.id);
-  let i;
-  rebel=0;
-for (i=0 ; i < users.length ; i++) {
- let   check = guild.members.get(users[i]);
-if(!check.voiceChannelID){
-        continue;
-}else{
-  rebel++;
-}
-}
-guild.channels.find('id', '464101304298504202')setName(" Voice「"+rebel+"」");
-  client.setInterval(() =>{
-    let d = Date.now()
-  }, 5000);
-});
-client.on('voiceStateUpdate', (oldMember, newMember) => {
-    let guild = client.guilds.get("399274624669581335");
-let newUserChannel = newMember.voiceChannel
-let oldUserChannel = oldMember.voiceChannel
- if(oldUserChannel === undefined && newUserChannel !== undefined) {
-   rebel++;
-guild.channels.find('id', '464101304298504202').setName(" Voice「"+rebel+"」");
-} else if(newUserChannel === undefined){
-  rebel--;
-guild.channels.find('id', '464101304298504202').setName(" Voice「"+rebel+"」");
-}
-});
-client.on('message', Codes => {
-  
-  if(Codes.content === "-صوت") {
-      Codes.channel.send(" Voice「"+rebel+"」");
-}
-});
+
 
 
 
