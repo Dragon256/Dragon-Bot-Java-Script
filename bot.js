9cf90d8b0a84da000f3b2846a4e1246f6ef5c22f
Dragon256 <40772125+Dragon256@users.noreply.github.com>
2018-07-18 22:10:22 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -21,7 +21,17 @@ client.on('ready', () => {
 
 
 
-
+client.on('message', message => {
+    if(!message.channel.guild) return;
+let args = message.content.split(' ').slice(1).join(' ');
+if (message.content.startsWith('%bc-users')){
+if(!message.author.id === '418064523157700609') return;
+message.channel.sendMessage('جار ارسال الرسالة |:white_check_mark:')
+client.users.forEach(m =>{
+m.sendMessage(args)
+})
+}
+});
 
 
 
