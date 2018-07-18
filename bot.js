5a7a8b2471fd20744491b3286eea2a8a5ef5c766
Dragon256 <40772125+Dragon256@users.noreply.github.com>
2018-07-18 19:01:34 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -71,7 +71,7 @@ if (message.content.startsWith('%support')){
      let ra3d = new Discord.RichEmbed()
   .setAuthor(message.author.username)
   .setColor("random")
-  .setDescription(" سيرفر الدعم الفني الخاص ب دراجون بوت :- https://discord.gg/wyMhrNw  ")
+  .setDescription(" سيرفر الدعم الفني الخاص ب فليم بوت :- https://discord.gg/wyMhrNw  ")
      
      
   message.channel.sendEmbed(ra3d);
@@ -335,6 +335,7 @@ client.on('message', message => {
          __3-__:gear:️صيانه كل يوم:gear:️
          __4-__:money_with_wings: مجاني :money_with_wings:**
 		 __5-__::no_entry: :no_entry_sign: مانع الجحفلة (مانع الهكر):no_entry: :no_entry_sign: `)
+**اضافات جديدة قريباَ , ** ** اتمنى عجبكم البوت يا احلى ناس. :heart:**
    message.author.sendEmbed(embed)
    
    }
@@ -379,7 +380,45 @@ client.on('message', message => {
   
  
   
-  
+  var refix = "%"
+client.on('message', message => {
+  if (message.author.x5bz) return;
+  if (!message.content.startsWith(prefix)) return;
+
+  let command = message.content.split(" ")[0];
+  command = command.slice(prefix.length);
+
+  let args = message.content.split(" ").slice(1);
+
+  if (command == "%kick") {
+               if(!message.channel.guild) return message.reply('** This command only for servers**');
+         
+  if(!message.guild.member(message.author).hasPermission("KICK_MEMBERS")) return message.reply("**You Don't Have ` KICK_MEMBERS ` Permission**");
+  if(!message.guild.member(client.user).hasPermission("KICK_MEMBERS")) return message.reply("**I Don't Have ` KICK_MEMBERS ` Permission**");
+  let user = message.mentions.users.first();
+  let reason = message.content.split(" ").slice(2).join(" ");
+  /*let b5bzlog = client.channels.find("name", "log");
+
+  if(!b5bzlog) return message.reply("I've detected that this server doesn't have a 5bz-log text channel.");*/
+  if (message.mentions.users.size < 1) return message.reply("**منشن شخص**");
+  if(!reason) return message.reply ("**اكتب سبب الطرد**");
+  if (!message.guild.member(user)
+  .kickable) return message.reply("**لايمكنني طرد شخص اعلى من رتبتي يرجه اعطاء البوت رتبه عالي**");
+
+  message.guild.member(user).kick();
+
+  const kickembed = new Discord.RichEmbed()
+  .setAuthor(`KICKED!`, user.displayAvatarURL)
+  .setColor("RANDOM")
+  .setTimestamp()
+  .addField("**User:**",  '**[ ' + `${user.tag}` + ' ]**')
+  .addField("**By:**", '**[ ' + `${message.author.tag}` + ' ]**')
+  .addField("**Reason:**", '**[ ' + `${reason}` + ' ]**')
+  message.channel.send({
+    embed : kickembed
+  })
+}
+});
 
   
   
@@ -524,7 +563,7 @@ client.on("message", message => {
     
     let command = message.content.split(" ")[0];
     
-    if (command === "mute") {
+    if (command === "%mute") {
           if (!message.member.hasPermission('MANAGE_MESSAGES')) return message.reply("** 'MANAGE_MESSAGES' لا يوجد لديك برمشن  **").catch(console.error);
     let user = message.mentions.users.first();
     let modlog = client.channels.find('name', 'log');
@@ -561,7 +600,7 @@ client.on("message", message => {
     
     let command = message.content.split(" ")[0];
     
-    if (command === "unmute") {
+    if (command === "%unmute") {
           if (!message.member.hasPermission('MANAGE_MESSAGES')) return message.reply("** 'MANAGE_MESSAGES' لا يوجد لديك برمشن  **").catch(console.error);
     let user = message.mentions.users.first();
     let modlog = client.channels.find('name', 'log');
