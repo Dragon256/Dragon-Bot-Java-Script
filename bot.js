321cd2468a24270bf53e78e759c789f9501ecff4
Dragon256 <40772125+Dragon256@users.noreply.github.com>
2018-07-19 16:11:44 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -20,6 +20,105 @@ client.on('ready', () => {
 
 
 
+client.on("message", (message) => {
+    const command = message.content.split(" ")[0];
+    const args = message.content.split(" ").slice();
+  if(command === "%deaf"){
+       let member = message.mentions.users.first(); 
+    message.guild.members.get(member.id).setDeaf(true);
+   }
+}).catch(console.error);
+
+
+
+
+
+
+const fs = require('fs')
+const Canvas = require('canvas')
+const jimp = require('jimp')
+const moment = require('moment')
+const prefix = '%';
+
+client.on('message', message => {
+    
+    let command = message.content.split(" ")[0];
+  command = command.slice(prefix.length);
+
+  let args = message.content.split(" ").slice(1);
+    let guild = (client, message).guild;
+     let user = message.mentions.users.first();
+     var men = message.mentions.users.first();
+        var heg;
+        if(men) {
+            heg = men
+        } else {
+            heg = message.author
+        }
+      var mentionned = message.mentions.members.first();
+         var h;
+        if(mentionned) {
+            h = mentionned
+        } else {
+            h = message.member
+        }
+       moment.locale('ar-TN');
+{
+  let args = message.content.split(" ").slice(1);
+if(command == "id") {
+    var Canvas = require('canvas')
+  , Image = new Canvas.Image
+  , canvas = new Canvas(450, 170)
+  , ctx = canvas.getContext('2d');
+  ctx.font = '15px Impact';
+  
+Image.src = canvas.toBuffer();
+
+
+    
+ctx.drawImage(Image, 0, 0, Image.width / 470, Image.height / 170);
+ctx.fillText(`${moment(heg.createdTimestamp).fromNow()}`,90, 20);
+ctx.fillText(`${moment(h.joinedAt).fromNow()}`,110, 70);
+ctx.fillText(`${heg.username}`,70, 40);
+ctx.fillText(`${h.presence.status}`,50, 70);
+
+
+ctx.beginPath();
+ctx.lineTo(50, 102);
+ctx.stroke();
+
+
+
+message.channel.sendFile(canvas.toBuffer());
+}
+}
+});
+
+
+
+
+
+
+
+
+
+
+client.on("message", (message) => {
+    const command = message.content.split(" ")[0];
+    const args = message.content.split(" ").slice();
+  if(command === "%undeaf"){
+       let member = message.mentions.users.first(); 
+    message.guild.members.get(member.id).setDeaf(null);
+   }
+}).catch(console.error);
+
+
+
+
+
+
+
+
 
 
 
@@ -1516,6 +1615,44 @@ client.on('message', message => {
                                                     message.channel.sendEmbed(embed);}
                                                   });
 
+
+
+
+
+
+
+
+
+
+
+const moment = require('moment');
+client.on('message', message => {
+             var prefix = "$"
+if (message.content.startsWith(prefix + "time")) {
+let user = message.mentions.users.first();
+var args = message.content.split(" ").slice(1);
+var men = message.mentions.users.first();
+var heg;
+if(men) {
+heg = men
+} else {
+heg = message.author
+}
+var mentionned = message.mentions.members.first();
+var h;
+if(mentionned) {
+h = mentionned
+} else {
+h = message.member
+}
+moment.locale('ar-TN'); //TN
+var id = new  Discord.RichEmbed()
+.setColor("RANDOM")
+.setAuthor(`${heg.username}#${heg.discriminator} `,heg.avatarURL)
+.setDescription([`**الوقت**${moment().format('HH:mm:ss A')}${moment().format('YYYY/M/D')}**اليوم** :${moment().format('dddd')}**التاريخ**`])
+message.channel.send(id)
+};
+});
 
 
 
