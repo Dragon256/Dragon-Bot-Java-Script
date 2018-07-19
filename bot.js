f5ba3754644811e85beacc4c146f2d73a733d12a
Dragon256 <40772125+Dragon256@users.noreply.github.com>
2018-07-19 16:56:11 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -123,36 +123,23 @@ client.on('message', message => {
 
 
 client.on('message', message => {
-    var prefix = "%"
-    let command = message.content.split(" ")[0];
-  command = command.slice(prefix.length);
-
-  let args = message.content.split(" ").slice(1);
-
-
-if(command == "draw") {
-    var Canvas = require('canvas')
-  , Image = new Canvas.Image
-  , canvas = new Canvas(450, 170)
-  , ctx = canvas.getContext('2d');
-  ctx.font = '30px Impact';
-  let args = message.content.split(" ").slice(1);
-  
-Image.src = canvas.toBuffer();
-
-    console.log(Image);
-ctx.drawImage(Image, 0, 0, Image.width / 470, Image.height / 170);
-ctx.fillText(args.join("  "),110, 70);
-
-
-ctx.beginPath();
-ctx.lineTo(50, 102);
-ctx.stroke();
-
-message.channel.sendFile(canvas.toBuffer());
-}
-}).on('ready', () => {
-
+    if(message.content.startsWith(prefix + 'draw')) { 
+        
+        var Canvas = require('canvas')
+        ,Image = new Canvas.Image
+        ,canvas = new Canvas(450, 170)
+        ,ctx = canvas.getContext('2d');
+        ctx.font = '40px Impact';
+        let args = message.content.split(" ").slice(1);
+        Image.src = canvas.toBuffer();
+        ctx.drawImage(Image, 0, 0, Image.width / 470, Image.height / 170);
+        ctx.fillText(args.join("  "),110, 70);
+        ctx.beginPath();
+        ctx.lineTo(50, 102);
+        ctx.stroke();
+        
+        message.channel.sendFile(canvas.toBuffer());
+    }
 });
 
 
@@ -162,7 +149,7 @@ message.channel.sendFile(canvas.toBuffer());
 
 
 client.on('message', message => {
-    if(message.content == '%bot-servers') {
+    if(message.content == '%servers-bot') {
              if(!message.author.id === '382293804671172620') return;
     var gimg;
     var gname;
