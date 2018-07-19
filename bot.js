c82266ef2cdb3674d69ef63ff7d5e13c357f91d3
Dragon256 <40772125+Dragon256@users.noreply.github.com>
2018-07-19 16:22:52 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -24,50 +24,6 @@ client.on('ready', () => {
 
 
 
-
-
-
-client.on('message', message => {
-    if (!message.channel.guild) return;
-    if(!message.member.hasPermission('ADMINISTRATOR')) return message.reply("***  ليس معك صلاحيات  ***")
-    var prefix = "%";
-       if (message.content ===   prefix + "add-role") {
-    message.channel.send("<@" + message.author.id + ">  *** جاري اعطاء الرتبة الى الأعضاء  *** ")
-    message.guild.members.forEach(m => {
-    m.addRole(message.guild.roles.find('name', 'Member'))
-    })
-    }
-    
-       if (message.content ===   prefix + "remove role") {
-    message.channel.send("<@" + message.author.id + ">  ***  جاري ازالة الرتبة من الأعضاء  *** ")
-    message.guild.members.forEach(m => {
-    m.removeRole(message.guild.roles.find('name', 'Member'))
-    })
-    }
-    
-    });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
   const math = require('math-expression-evaluator');
 const stripIndents = require('common-tags').stripIndents;
 
