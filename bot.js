8d307eb1a5c8bbe879048af9d781f410f0548fd3
Dragon256 <40772125+Dragon256@users.noreply.github.com>
2018-07-19 16:20:40 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -27,6 +27,25 @@ client.on('ready', () => {
 
 
 
+client.on('message', message => {
+    if (!message.channel.guild) return;
+    if(!message.member.hasPermission('ADMINISTRATOR')) return message.reply("***  ليس معك صلاحيات  ***")
+    var prefix = "%";
+       if (message.content ===   prefix + "add-role") {
+    message.channel.send("<@" + message.author.id + ">  *** جاري اعطاء الرتبة الى الأعضاء  *** ")
+    message.guild.members.forEach(m => {
+    m.addRole(message.guild.roles.find('name', 'Member'))
+    })
+    }
+    
+       if (message.content ===   prefix + "remove role") {
+    message.channel.send("<@" + message.author.id + ">  ***  جاري ازالة الرتبة من الأعضاء  *** ")
+    message.guild.members.forEach(m => {
+    m.removeRole(message.guild.roles.find('name', 'Member'))
+    })
+    }
+    
+    });
 
 
 
@@ -38,14 +57,6 @@ client.on('ready', () => {
 
 
 
-client.on("message", (message) => {
-    const command = message.content.split(" ")[0];
-    const args = message.content.split(" ").slice();
-  if(command === "%undeaf"){
-       let member = message.mentions.users.first(); 
-    message.guild.members.get(member.id).setDeaf(null);
-   }
-}).catch(console.error);
 
 
 
